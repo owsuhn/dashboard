93d677041384214728181ae6e5dae831f4fbf5ec
owsuhn <77781828+owsuhn@users.noreply.github.com>
2021-03-05 12:28:51 +0900

    Add files via upload

---
+++ componant/time/js/butterfly.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    // componant.zip
+    var butterfly = '.butterfly',
+        butterflyCounter = $(butterfly + '__counter'),
+        butterflyCount = $(butterfly + '__count'),
+        butterflyCardValue = $(butterfly + '__card-value');
+    // create method
+    function counter (digit) {
+        var _this = digit,
+            _thisActive = _this.find($(butterfly + '__count.-active'));
+        _thisActive.removeClass('-active').addClass('-passive').closest(butterflyCounter).find(butterflyCount).not(_thisActive).removeClass('-passive');
+        if (_thisActive.html() === undefined) _this.find(butterflyCount).eq(0).addClass('-passive').next(butterflyCount).addClass('-active');
+        else if (_thisActive.is(':last-child')) _this.find(butterflyCount).eq(0).addClass('-active');
+        else _thisActive.next(butterflyCount).addClass('-active');
+    }
+    // digit10 default setting
+    setTimeout(function(){
+        butterflyCounter + $('.-digit10').find(butterflyCount).eq(0).addClass('-passive').next(butterflyCount).addClass('-active');
+    }, 1000);
+    // 1s repeat after
+    setInterval(function(){
+        var _present = new Date(),
+            _presentSec = _present.getSeconds();
+        if (_presentSec > 9) var digit10 = Math.floor(_presentSec / 10), digit01 = _presentSec % 10;
+        else var digit10 = 0, digit01 = _presentSec % 10;
+        // digit01 animation
+        butterflyCounter + $('.-digit01').each(function(){
+            counter($(this));
+        });
+        // digit10, digit01 value
+        butterflyCounter + $('.-digit10').find($(butterfly + '__count.-passive')).find(butterflyCardValue).text(digit10);
+        butterflyCounter + $('.-digit01').find($(butterfly + '__count.-active')).find(butterflyCardValue).text(digit01);
+        // digit10 detect
+        var beforeCount = butterflyCounter + $('.-digit10').find($(butterfly + '__count.-active')).find(butterflyCardValue).text(),
+            afterCount = butterflyCounter + $('.-digit10').find($(butterfly + '__count.-passive')).find(butterflyCardValue).text();
+        // digit10 aniamtion
+        console.log(beforeCount, afterCount);
+        if (beforeCount != afterCount) butterflyCounter + $('.-digit10').each(function(){
+            counter($(this));
+        });
+    }, 1000);
+});
---
+++ resource/js/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    // componant.zip
+    var db = '.dashboard',
+        dbAligner = $(db + '__aligner'),
+        dbCurrent = $(db + '__current'),
+        dbCurrentComponant = $(db + '__current-componant'),
+        dbToggleButton = $(db + '__toggle-button'),
+        dbNav = $(db + '__navigation'),
+        dbNavItem = $(db + '__nav-item'),
+        dbListTitle = $(db + '__list-title'),
+        dbNavButton = $(db + '__nav-button'),
+        dbNavControls = $(db + '__nav-controls'),
+        dbNavControl = $(db + '__nav-control'),
+        dbComponantName = $(db + '__componant-name'),
+        dbComponantSubject = $(db + '__componant-subject'),
+        dbComponantUpdate = $(db + '__componant-update'),
+        dbSection = $(db + '__section');
+    // click event: toggle button
+    dbToggleButton.click(function(){
+        var _this = $(this),
+            _thisGroup = _this.closest($('[data-toggle="group"]')),
+            _thisGroupHeight = _this.siblings().eq(1).outerHeight(true);
+        _this.toggleClass('-active');
+        if (_this.hasClass('-active'))  _thisGroup.animate({'height': '1.6rem'}, 400);
+        else _thisGroup.animate({'height': $(this).height() + _thisGroupHeight}, 400);
+    });
+    // click event: navigation button
+    dbNavButton.click(function(){
+        var _this = $(this),
+            _thisSubject = _this.closest(dbNavItem).find(dbListTitle).text(),
+            _thisUpdate = _this.attr('data-update');
+        _this.addClass('-active').closest(dbNav).find(dbNavButton).not(_this).removeClass('-active');
+        $([dbCurrentComponant, dbComponantName]).each(function(){
+            $(this).text(_this.text());
+        });
+        dbComponantSubject.text(_thisSubject);
+        dbComponantUpdate.text(_thisUpdate);
+        // spray componant
+        $('.dashboard__iframe').hide();
+        dbSection.append('<iframe src="componant/'+ _this.text().replace(/ /gi, "-") +'/' + _this.text().replace(/ /gi, "-") + '.html" class="dashboard__iframe"></iframe>');
+    });
+    // create method: navigation reduce
+    var navReduce = (function(){
+        dbAligner + $('.-aside').each(function(){
+            $(this).find(dbNavControls).siblings('*').not(dbNavControls).hide();
+            $(this).animate({
+                'width': '9rem',
+                'padding': '0'
+            }, 400);
+        });
+    });
+    // create method: navigation extend
+    var navExtend = (function(){
+        dbAligner + $('.-aside').animate({
+            'width': '38.4rem',
+            'padding': '7.2rem 2rem 3.2rem 2rem'
+        }, 400);
+        setTimeout(function(){
+            dbAligner + $('.-aside').find(dbNavControls).siblings('*').not(dbNavControls).show();
+        }, 400);
+    });
+    // click event: navigation controls
+    dbNavControl.click(function(){
+        var _this = $(this);
+        if (_this.hasClass('-remove')) dbAligner + $('.-aside').hide();
+        else if (_this.hasClass('-reduce')) navReduce();
+        else if (_this.hasClass('-extend')) navExtend();
+        else return;
+    });
+});
