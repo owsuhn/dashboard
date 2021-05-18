00159de3c5165390bc33edce541c6b15d722915f
owsuhn <77781828+owsuhn@users.noreply.github.com>
2021-05-18 15:19:40 +0900

    Delete componant/switch directory

--- componant/switch/js/switch.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    // componant.zip
-    var swc = '.switch', // "switch" is not allowed var name
-        swcButton = $(swc + '__button');
-    swcButton.click(function(){
-        $(this).attr('aria-pressed', $(this).attr('aria-pressed') === 'false' ? 'true' : 'false');
-    });
-});
